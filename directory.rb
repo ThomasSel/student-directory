5e795828553ef3ded07ac04b225a4fc98b7b335b
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-10-31 10:52:24 +0000

    Asks user for filename before doing save/load

--- directory.rb
+++ directory.rb
@@ -23,12 +23,12 @@ def interactive_menu
   end
 end
 
-def try_load_students
+def load_students_from_commandline
   filename = ARGV.first # First argument from the command line
-  return if filename.nil? # Get out of the method if it isn't given
+  filename = "students.csv" if filename.nil? # Get out of the method if it isn't given
   if File.exist?(filename) # If it exists
     load_students(filename)
-    puts "Loaded #{@students.count} from #{filename}"
+    puts "Loaded #{@students.count} #{@students.count == 1 ? "student" : "students"} from #{filename}"
   else # If it doesn't exist
     puts "Sorry, #{filename} doesn't exist."
     exit # Quit the program
@@ -36,25 +36,26 @@ def try_load_students
 end
 
 def print_menu
-  puts "1. Input the students"
+  puts "\n1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
+  puts "3. Save the list to file"
+  puts "4. Load the list from file"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
-def show_students
+def print_directory
   print_header
   print_students_list
   print_footer
 end
 
 def process(selection)
+  puts ""
   case selection
     when "1"
       input_students
     when "2"
-      show_students
+      print_directory
     when "3"
       save_students
     when "4"
@@ -66,25 +67,39 @@ def process(selection)
   end
 end
 
-def save_students
-  # Open the file for writing
-  file = File.open("students.csv", "w")
-  # Iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+def get_filename(check_if_exists: true)
+  puts "Please input the file you wish to perform this action on"
+  filename = gets.chomp
+  while check_if_exists && !File.exists?(filename)
+    puts "The file #{filename} doesn't exist. Please input the file name again"
+    filename = STDIN.gets.chomp
   end
-  file.close
+  filename
 end
 
-def load_students(filename="students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(',')
-    @students << { name: name, cohort: cohort.to_sym }
+def save_students
+  filename = get_filename(check_if_exists: false)
+  # Open the file for writing
+  File.open(filename, "w") do |file|
+    # Iterate over the array of students
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
   end
-  file.close
+  puts "Succesfully saved students to #{filename}"
+end
+
+def load_students(filename=nil)
+  filename ||= get_filename
+  File.open(filename, "r") do |file|
+    file.readlines.each do |line|
+      name, cohort = line.chomp.split(',')
+      @students << { name: name, cohort: cohort.to_sym }
+    end
+  end
+  puts "Successfully loaded students from #{filename}"
 end
 
 def print_header
@@ -102,5 +117,5 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
-try_load_students
+load_students_from_commandline
 interactive_menu
